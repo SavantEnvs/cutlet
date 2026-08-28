dd8b6a12b2aadab8237a527ab827b4e5c44b2975
David Brumley <2975959+dbrumley@users.noreply.github.com>
2026-08-28 14:02:06 +0000

    Mayhem integration: cutlet romaji_decoder Atheris harness (spec v2)

---
+++ mayhem/fuzz/romaji_decoder.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+import atheris
+import sys
+with atheris.instrument_imports():
+    import cutlet
+
+katsu = cutlet.Cutlet()
+kunrei = cutlet.Cutlet("kunrei")
+nihon = cutlet.Cutlet("nihon")
+nippon = cutlet.Cutlet('nippon')
+hepburn = cutlet.Cutlet("hepburn")
+
+@atheris.instrument_func
+def TestOneInput(data):
+    fdp = atheris.FuzzedDataProvider(data)
+
+    if len(data) < 1:
+        return
+
+    option = fdp.ConsumeBytes(1)[0]
+    in_string = fdp.ConsumeUnicodeNoSurrogates(len(data))
+
+    if option % 5 == 0:
+        katsu.romaji(in_string)
+    elif option % 5 == 1:
+        kunrei.romaji(in_string)
+    elif option % 5 == 2:
+        nihon.romaji(in_string)
+    elif option % 5 == 3:
+        nippon.romaji(in_string)
+    else:
+        hepburn.romaji(in_string)
+
+atheris.Setup(sys.argv, TestOneInput)
+atheris.Fuzz()
